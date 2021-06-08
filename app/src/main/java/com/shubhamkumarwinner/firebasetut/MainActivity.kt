b8ad6d41ff9f6cf88669085e68cce851607c7694
Shubham <shubhamkumarwinner@gmail.com>
2021-06-08 19:27:23 +0530

    Adding google sign in

--- app/src/main/java/com/shubhamkumarwinner/firebasetut/MainActivity.kt
+++ app/src/main/java/com/shubhamkumarwinner/firebasetut/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.shubhamkumarwinner.firebasetut
 
+import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.UserProfileChangeRequest
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.CoroutineScope
@@ -12,15 +17,15 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.withContext
-
+const val REQUEST_CODE_SIGN_IN = 0
 class MainActivity : AppCompatActivity() {
-    lateinit var auth: FirebaseAuth
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         auth = FirebaseAuth.getInstance()
 
-        //for example
+        /*//for example
 //        auth.signOut()
 
         //for registration
@@ -36,11 +41,52 @@ class MainActivity : AppCompatActivity() {
         //for updating profile image and user name
         btnUpdateProfile.setOnClickListener {
             updateProfile()
+        }*/
+
+        //for google sign in
+        btnGoogleSignIn.setOnClickListener {
+            val options = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.web_client_id))
+                .requestEmail()
+                .build()
+            val signInClient = GoogleSignIn.getClient(this, options)
+            signInClient.signInIntent.also {
+                startActivityForResult(it, REQUEST_CODE_SIGN_IN)
+            }
+        }
+    }
+
+    // for google sign in
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CODE_SIGN_IN){
+            val account = GoogleSignIn.getSignedInAccountFromIntent(data).result
+            account?.let {
+                googleAuthForFirebase(it)
+            }
+        }
+    }
+
+    //for google sign in
+    private fun googleAuthForFirebase(account: GoogleSignInAccount) {
+        val credentials = GoogleAuthProvider.getCredential(account.idToken, null)
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                auth.signInWithCredential(credentials).await()
+                withContext(Dispatchers.Main){
+                    Toast.makeText(this@MainActivity, "Successfully logged in", Toast.LENGTH_LONG).show()
+                }
+
+            }catch (e: Exception){
+                withContext(Dispatchers.Main){
+                    Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_LONG).show()
+                }
+            }
         }
     }
 
     //for adding profile pic and username
-    private fun updateProfile(){
+    /*private fun updateProfile(){
         auth.currentUser?.let {user ->
             val userName = etUsername.text.toString()
             val photoURI = Uri.parse("android.resource://$packageName/${R.drawable.pic}")
@@ -121,5 +167,5 @@ class MainActivity : AppCompatActivity() {
             etUsername.setText(user.displayName)
             ivProfilePicture.setImageURI(user.photoUrl)
         }
-    }
+    }*/
 }
