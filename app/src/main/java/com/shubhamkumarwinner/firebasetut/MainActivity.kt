24d44b4b0b171bac21b3977e276cac91b178f75e
Shubham <shubhamkumarwinner@gmail.com>
2021-06-14 17:59:20 +0530

    Firebase storage upload and download image

--- app/src/main/java/com/shubhamkumarwinner/firebasetut/MainActivity.kt
+++ app/src/main/java/com/shubhamkumarwinner/firebasetut/MainActivity.kt
@@ -1,171 +1,89 @@
 package com.shubhamkumarwinner.firebasetut
 
 import android.content.Intent
+import android.graphics.BitmapFactory
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.GoogleAuthProvider
-import com.google.firebase.auth.UserProfileChangeRequest
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.withContext
-const val REQUEST_CODE_SIGN_IN = 0
+import kotlin.math.max
+
+private const val REQUEST_CODE_IMAGE_PICK = 0
 class MainActivity : AppCompatActivity() {
-    private lateinit var auth: FirebaseAuth
+    var curFile: Uri? = null
+
+    val imageRef = Firebase.storage.reference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        auth = FirebaseAuth.getInstance()
 
-        /*//for example
-//        auth.signOut()
-
-        //for registration
-        btnRegister.setOnClickListener {
-            registerUser()
+        // for choosing image from gallery
+        ivImage.setOnClickListener {
+            Intent(Intent.ACTION_GET_CONTENT).also {
+                it.type = "image/*"
+                startActivityForResult(it, REQUEST_CODE_IMAGE_PICK)
+            }
         }
 
-        //for login
-        btnLogin.setOnClickListener {
-            loginUser()
+        // for uploading image
+        btnUploadImage.setOnClickListener {
+            uploadImageToStorage("myImage")
         }
 
-        //for updating profile image and user name
-        btnUpdateProfile.setOnClickListener {
-            updateProfile()
-        }*/
+        // for downloading image
+        btnDownloadImage.setOnClickListener {
+            downloadImage("myImage")
+        }
+    }
 
-        //for google sign in
-        btnGoogleSignIn.setOnClickListener {
-            val options = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.web_client_id))
-                .requestEmail()
-                .build()
-            val signInClient = GoogleSignIn.getClient(this, options)
-            signInClient.signInIntent.also {
-                startActivityForResult(it, REQUEST_CODE_SIGN_IN)
+    // for downloading image
+    private fun downloadImage(fileName: String) = CoroutineScope(Dispatchers.IO).launch {
+        try {
+            val maxDownloadSize = 5L * 1024 * 1024
+            val bytes = imageRef.child("images/$fileName").getBytes(maxDownloadSize).await()
+            val bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
+            withContext(Dispatchers.Main){
+                ivImage.setImageBitmap(bitmap)
+            }
+        }catch (e: Exception){
+            withContext(Dispatchers.Main){
+                Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_LONG).show()
             }
         }
     }
 
-    // for google sign in
+    // for uploading image
+    private fun uploadImageToStorage(fileName: String) = CoroutineScope(Dispatchers.IO).launch {
+        try {
+            curFile?.let{
+                imageRef.child("images/$fileName").putFile(it).await()
+                withContext(Dispatchers.Main){
+                    Toast.makeText(this@MainActivity, "Successfully uploaded", Toast.LENGTH_LONG).show()
+                }
+            }
+        }catch (e: Exception){
+            withContext(Dispatchers.Main){
+                Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == REQUEST_CODE_SIGN_IN){
-            val account = GoogleSignIn.getSignedInAccountFromIntent(data).result
-            account?.let {
-                googleAuthForFirebase(it)
+        if (resultCode == RESULT_OK && requestCode == REQUEST_CODE_IMAGE_PICK){
+            data?.data?.let {
+                curFile = it
+                ivImage.setImageURI(it)
             }
         }
     }
-
-    //for google sign in
-    private fun googleAuthForFirebase(account: GoogleSignInAccount) {
-        val credentials = GoogleAuthProvider.getCredential(account.idToken, null)
-        CoroutineScope(Dispatchers.IO).launch {
-            try {
-                auth.signInWithCredential(credentials).await()
-                withContext(Dispatchers.Main){
-                    Toast.makeText(this@MainActivity, "Successfully logged in", Toast.LENGTH_LONG).show()
-                }
-
-            }catch (e: Exception){
-                withContext(Dispatchers.Main){
-                    Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_LONG).show()
-                }
-            }
-        }
-    }
-
-    //for adding profile pic and username
-    /*private fun updateProfile(){
-        auth.currentUser?.let {user ->
-            val userName = etUsername.text.toString()
-            val photoURI = Uri.parse("android.resource://$packageName/${R.drawable.pic}")
-            val profileUpdates = UserProfileChangeRequest.Builder()
-                .setDisplayName(userName)
-                .setPhotoUri(photoURI)
-                .build()
-
-            CoroutineScope(Dispatchers.IO).launch {
-                try {
-                    user.updateProfile(profileUpdates).await()
-                    withContext(Dispatchers.Main){
-                        checkLoggedInState()
-                        Toast.makeText(this@MainActivity, "Successfully updated user profile", Toast.LENGTH_LONG).show()
-                    }
-                }catch (e: Exception){
-                    withContext(Dispatchers.Main){
-                        Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_LONG).show()
-                    }
-                }
-            }
-        }
-    }
-
-    override fun onStart() {
-        super.onStart()
-        checkLoggedInState()
-    }
-
-    private fun registerUser(){
-        val email  = etEmailRegister.text.toString()
-        val password  = etPasswordRegister.text.toString()
-
-        if (email.isNotEmpty() && password.isNotEmpty()){
-            CoroutineScope(Dispatchers.IO).launch {
-                try {
-                    auth.createUserWithEmailAndPassword(email, password).await()
-                    withContext(Dispatchers.Main){
-                        checkLoggedInState()
-                    }
-                }
-                catch (e: Exception){
-                    withContext(Dispatchers.Main){
-                        Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_LONG).show()
-                    }
-                }
-            }
-        }
-    }
-
-    private fun loginUser(){
-        val email  = etEmailLogin.text.toString()
-        val password  = etPasswordLogin.text.toString()
-
-        if (email.isNotEmpty() && password.isNotEmpty()){
-            CoroutineScope(Dispatchers.IO).launch {
-                try {
-                    auth.signInWithEmailAndPassword(email, password).await()
-                    withContext(Dispatchers.Main){
-                        checkLoggedInState()
-                    }
-                }
-                catch (e: Exception){
-                    withContext(Dispatchers.Main){
-                        Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_LONG).show()
-                    }
-                }
-            }
-        }
-    }
-
-    private fun checkLoggedInState() {
-        val user = auth.currentUser
-        if (user == null){
-            tvLoggedIn.text = "You are not logged in"
-        }else{
-            tvLoggedIn.text = "You are logged in"
-            etUsername.setText(user.displayName)
-            ivProfilePicture.setImageURI(user.photoUrl)
-        }
-    }*/
 }
