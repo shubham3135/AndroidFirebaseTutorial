4aad655c84bb847be139050ac6add4464401d2f7
Shubham <shubhamkumarwinner@gmail.com>
2021-06-16 23:19:34 +0530

    Upload data to firestore

--- app/src/main/java/com/shubhamkumarwinner/firebasetut/MainActivity.kt
+++ app/src/main/java/com/shubhamkumarwinner/firebasetut/MainActivity.kt
@@ -1,132 +1,41 @@
 package com.shubhamkumarwinner.firebasetut
 
-import android.content.Intent
-import android.graphics.BitmapFactory
-import android.net.Uri
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
-import com.google.firebase.storage.ktx.storage
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.withContext
-import kotlin.math.max
 
-private const val REQUEST_CODE_IMAGE_PICK = 0
 class MainActivity : AppCompatActivity() {
-    var curFile: Uri? = null
-
-    val imageRef = Firebase.storage.reference
-
+    private val personCollectionRef = Firebase.firestore.collection("persons")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        // for choosing image from gallery
-        ivImage.setOnClickListener {
-            Intent(Intent.ACTION_GET_CONTENT).also {
-                it.type = "image/*"
-                startActivityForResult(it, REQUEST_CODE_IMAGE_PICK)
-            }
+        btnUploadData.setOnClickListener {
+            val firstName = etFirstName.text.toString()
+            val lastName = etLastName.text.toString()
+            val age = etAge.text.toString().toInt()
+            val person = Person(firstName, lastName, age)
+            savePerson(person)
         }
-
-        // for uploading image
-        btnUploadImage.setOnClickListener {
-            uploadImageToStorage("myImage${System.currentTimeMillis()}")
-        }
-
-        // for downloading image
-        btnDownloadImage.setOnClickListener {
-            downloadImage("myImage")
-        }
-
-        // for deleting image
-        btnDeleteImage.setOnClickListener {
-            deleteImage("myImage")
-        }
-
-        listFiles()
     }
 
-    private fun listFiles() = CoroutineScope(Dispatchers.IO).launch {
+    // upload data to firestore
+    private fun savePerson(person: Person) = CoroutineScope(Dispatchers.IO).launch {
         try {
-            val images = imageRef.child("images/").listAll().await()
-            val imageUrls = mutableListOf<String>()
-            for (image in images.items){
-                val url = image.downloadUrl.await()
-                imageUrls.add(url.toString())
-            }
+            personCollectionRef.add(person).await()
             withContext(Dispatchers.Main){
-                val imageAdapter = ImageAdapter(imageUrls)
-                rvImages.apply {
-                    adapter = imageAdapter
-                    layoutManager = LinearLayoutManager(this@MainActivity)
-                }
+                Toast.makeText(this@MainActivity, "Successfully saved data.", Toast.LENGTH_LONG).show()
             }
         }catch (e: Exception){
             withContext(Dispatchers.Main){
                 Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_LONG).show()
-            }
-        }
-    }
-
-    // for deleting image
-    private fun deleteImage(fileName: String) = CoroutineScope(Dispatchers.IO).launch{
-        try {
-            imageRef.child("images/$fileName").delete().await()
-            withContext(Dispatchers.Main){
-                Toast.makeText(this@MainActivity, "Successfully deleted image", Toast.LENGTH_LONG).show()
-            }
-        }catch (e: Exception){
-            withContext(Dispatchers.Main){
-                Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_LONG).show()
-            }
-        }
-    }
-
-    // for downloading image
-    private fun downloadImage(fileName: String) = CoroutineScope(Dispatchers.IO).launch {
-        try {
-            val maxDownloadSize = 5L * 1024 * 1024
-            val bytes = imageRef.child("images/$fileName").getBytes(maxDownloadSize).await()
-            val bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
-            withContext(Dispatchers.Main){
-                ivImage.setImageBitmap(bitmap)
-            }
-        }catch (e: Exception){
-            withContext(Dispatchers.Main){
-                Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_LONG).show()
-            }
-        }
-    }
-
-    // for uploading image
-    private fun uploadImageToStorage(fileName: String) = CoroutineScope(Dispatchers.IO).launch {
-        try {
-            curFile?.let{
-                imageRef.child("images/$fileName").putFile(it).await()
-                withContext(Dispatchers.Main){
-                    Toast.makeText(this@MainActivity, "Successfully uploaded", Toast.LENGTH_LONG).show()
-                }
-            }
-        }catch (e: Exception){
-            withContext(Dispatchers.Main){
-                Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_LONG).show()
-            }
-        }
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (resultCode == RESULT_OK && requestCode == REQUEST_CODE_IMAGE_PICK){
-            data?.data?.let {
-                curFile = it
-                ivImage.setImageURI(it)
             }
         }
     }
---
+++ app/src/main/java/com/shubhamkumarwinner/firebasetut/Person.kt
@@ -0,0 +1,7 @@
+package com.shubhamkumarwinner.firebasetut
+
+data class Person(
+    var firstName: String,
+    var lastName: String,
+    var age: Int
+)
